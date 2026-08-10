6153738efef0f0ebe5d0bddb6ff9480f6f68dc70
ravinahardkoretech-rs <ravina.hardkore.tech@gmail.com>
2026-08-10 12:43:12 +0530

    Update authController.js

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -9,7 +9,10 @@ console.log("EMAIL_USER:", process.env.EMAIL_USER);
 console.log("EMAIL_PASS exists:", !!process.env.EMAIL_PASS);
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  requireTLS: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
