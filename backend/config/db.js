d509b9d9374184b42c90e2e1d8cc84d00cd43607
ravinahardkoretech-rs <ravina.hardkore.tech@gmail.com>
2026-08-10 14:57:15 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,9 +2,10 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-
-    await mongoose.connect(process.env.MONGO_URI
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB Connected");
   } catch (error) {
+    console.error("MongoDB connection error:", error.message);
     process.exit(1);
   }
 };
