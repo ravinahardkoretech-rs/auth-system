8201c95052576a64c4c9a97d239a617c4924e4a7
ravinahardkoretech-rs <ravina.hardkore.tech@gmail.com>
2026-08-19 14:46:33 +0530

    Update sendEmail.js

--- backend/config/sendEmail.js
+++ backend/config/sendEmail.js
@@ -3,13 +3,11 @@ const nodemailer = require("nodemailer");
 const sendEmail = async (email, subject, text) => {
 const transporter = nodemailer.createTransport({
 service: "gmail",
-
-```
+  
 auth: {
   user: process.env.EMAIL_USER,
   pass: process.env.EMAIL_PASS,
 },
-```
 
 });
 
